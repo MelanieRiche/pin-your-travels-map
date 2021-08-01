a8ecccd4a756c3250cce3f316a02f8a50df10160
Melanie <contact.mriche@gmail.com>
2021-08-01 22:05:13 +0200

    Install Mapbox + toekn access

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
   return (
-    <div className="App">
-      hello
-    </div>
+    <ReactMapGL
+    {...viewport}
+    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+    onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
   );
 }
 
