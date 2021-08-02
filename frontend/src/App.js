1d08c2641d2d66ece63dad3137b8777a0e5d5224
Melanie <contact.mriche@gmail.com>
2021-08-02 02:08:02 +0200

    Open and close pin card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import ReactMapGL, { Marker, Popup } from 'react-map-gl';
-import { Room, Star } from '@material-ui/icons';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
 import axios from "axios";
 import { format } from "timeago.js";
 
-import "./app.css"
+import "./app.css";
 
 function App() {
   const [pins, setPins] = useState([]);
-
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
     latitude: 48,
     longitude: 10,
-    zoom: 4
+    zoom: 4,
   });
 
   useEffect(() => {
@@ -29,56 +29,61 @@ function App() {
     getPins();
   }, []);
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
+
   return (
     <ReactMapGL
-    {...viewport}
-    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-    onViewportChange={nextViewport => setViewport(nextViewport)}
-    mapStyle="mapbox://styles/rocknsoph/ckrtnkv1c8qlk19o1d6j01kkx"
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      mapStyle="mapbox://styles/rocknsoph/ckrtnkv1c8qlk19o1d6j01kkx"
     >
-
       {pins.map((p) => (
-            <>
-              <Marker
+        <>
+          <Marker latitude={p.lat} longitude={p.long}>
+            <Room
+              style={{
+                fontSize: viewport.zoom * 7,
+                color: "#FF3864",
+              }}
+              onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+            />
+          </Marker>
+          {p._id === currentPlaceId && (
+            <Popup
               latitude={p.lat}
               longitude={p.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setCurrentPlaceId(null)}
+              anchor="bottom"
             >
-              <Room 
-                style={{
-                  fontSize: viewport.zoom * 7,
-                  color: "#FF3864",
-                }}
-              />
-            </Marker>
-        <Popup
-            latitude={p.lat}
-            longitude={p.long}
-            closeButton={true}
-            closeOnClick={false}
-            anchor="bottom"
-            >
-            <div className="card">
-            <label>Place</label>
-              <h4 className="place">{p.title}</h4>
-              <label>Review</label>
-              <p className="desc">{p.desc}</p>
-              <label>Rating</label>
-              <div className="stars">
-                <Star className="star" />
-                <Star className="star" />
-                <Star className="star" />
-                <Star className="star" />
-                <Star className="star" />
+              <div className="card">
+                <label>Place</label>
+                <h4 className="place">{p.title}</h4>
+                <label>Review</label>
+                <p className="desc">{p.desc}</p>
+                <label>Rating</label>
+                <div className="stars">
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                  <Star className="star" />
+                </div>
+                <label>Information</label>
+                <span className="username">
+                  Created by <b>{p.username}</b>
+                </span>
+                <span className="date">{format(p.createdAt)}</span>
               </div>
-              <label>Information</label>
-              <span className="username">Created by <b>{p.username}</b></span>
-              <span className="date">{format(p.createdAt)}</span>
-
-            </div>
             </Popup>
-                    </>
-          ))}
-
+          )}
+        </>
+      ))}
     </ReactMapGL>
   );
 }
